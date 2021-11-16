b7ae98d6cd3d0db3fee21477126892402da17775
John Patrick Roldan <91767440+roldanjohn@users.noreply.github.com>
2021-11-16 20:08:55 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const preview = document.querySelector('#project-preview');
+const projectSelect = document.querySelectorAll('.project-desc');
+
+projectSelect.forEach(project => {
+  project.addEventListener('mouseenter', (e) => {
+    let imageName = e.target.getAttribute('data-id');
+    preview.style.backgroundImage = `url('img/${imageName}.JPG')`;
+  });
+  project.addEventListener('mouseleave', () => {
+    preview.style.backgroundImage = 'none';
+  });
+});

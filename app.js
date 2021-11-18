b5004a58e03a5ba8e43260caa2f53cf3af4a320d
roldanjohn <roldanjohnpatrick@gmail.com>
2021-11-18 16:21:41 +0800

    Added intersection observer

--- app.js
+++ app.js
@@ -6,7 +6,28 @@ projectSelect.forEach(project => {
     let imageName = e.target.getAttribute('data-id');
     preview.style.backgroundImage = `url('img/${imageName}.JPG')`;
   });
-  // project.addEventListener('mouseleave', () => {
-  //   preview.style.backgroundImage = 'none';
-  // });
 });
+
+
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.7
+}
+const callback = function(entries){
+  entries.forEach(entry => {
+    if(!entry.isIntersecting){
+      return;
+    }
+    let theKids = entry.target.children;
+    Array.from(theKids).forEach(x => {
+      x.classList.toggle('inactive');
+    })
+    observer.unobserve(entry.target);
+  });
+};
+const observer = new IntersectionObserver(callback, options);
+
+document.querySelectorAll('section').forEach(div => {
+  observer.observe(div);
+})
